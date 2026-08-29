597aac7af50fbb3f11d3b2bd1743ea125b117ac0
VishnavVijayanand <vishnav.vijayanand@gmail.com>
2026-08-29 10:43:51 +0000

    lab01 task2

--- labs/lab01/task2/FA_Gate.v
+++ labs/lab01/task2/FA_Gate.v
@@ -26,10 +26,10 @@ module FA_Gate(
 );
   wire ps, pc1, pc2;
 
-  xor (ps,  a,   b);
-  and (pc1, a,   b);
-  xor (sum, cin, ps);
-  and (pc2, cin, ps);
-  or  (cout, pc1, pc2);
+  xor #(2,5)(ps,  a,   b);
+  and #(2,5)(pc1, a,   b);
+  xor #(2,5)(sum, cin, ps);
+  and #(2,5)(pc2, cin, ps);
+  or  #(2,5)(cout, pc1, pc2);
 
 endmodule
